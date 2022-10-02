1a84baf083fc52f9484e8d0a2fe4d9c93e8676f2
Dingkun Yang <81750079+Yer1k@users.noreply.github.com>
2022-10-02 19:54:57 -0400

    Telegram Bot
    - able to scrap all message from specific channel
    - please input your own account credential to test it

---
+++ README.md
@@ -0,0 +1,5 @@
+# telbot_DY
+
+[![Python application test with Github Actions using devcontainers](https://github.com/nogibjj/telbot_DY/actions/workflows/main.yml/badge.svg)](https://github.com/nogibjj/telbot_DY/actions/workflows/main.yml)
+
+Telegram bot
---
+++ scrap_message.py
@@ -0,0 +1,106 @@
+import os
+from datetime import date, datetime
+import pandas as pd
+import json
+from telethon.sync import TelegramClient
+from telethon.errors import SessionPasswordNeededError
+from telethon.tl.functions.messages import (GetHistoryRequest)
+from telethon.tl.types import (
+    PeerChannel
+)
+
+api_id = os.getenv('API_ID')
+api_hash = os.getenv('API_HASH')
+
+phone = os.getenv('PHONE')
+username = os.getenv('USERNAME')
+
+client = TelegramClient(username, api_id, api_hash)
+
+# Scrap Channel Messages
+class DateTimeEncoder(json.JSONEncoder):
+    def default(self, o):
+        if isinstance(o, datetime):
+            return o.isoformat()
+
+        if isinstance(o, bytes):
+            return list(o)
+
+        return json.JSONEncoder.default(self, o)
+
+
+async def main(phone):
+    await client.start()
+    print("Client Created")
+    # Ensure you're authorized
+    if await client.is_user_authorized() == False:
+        await client.send_code_request(phone)
+        try:
+            await client.sign_in(phone, input('Enter the code: '))
+        except SessionPasswordNeededError:
+            await client.sign_in(password=input('Password: '))
+
+    me = await client.get_me()
+
+    entity = 'https://t.me/OFFICIALairdropalert'
+
+    # if user_input_channel.isdigit():
+    #     entity = PeerChannel(int(user_input_channel))
+    # else:
+    #     entity = user_input_channel
+
+    my_channel = await client.get_entity(entity)
+
+    offset_id = 0
+    limit = 100
+    all_messages = []
+    total_messages = 0
+    total_count_limit = 0
+
+    while True:
+        print("Current Offset ID is:", offset_id, "; Total Messages:", total_messages)
+        history = await client(GetHistoryRequest(
+            peer=my_channel,
+            offset_id=offset_id,
+            offset_date=None,
+            add_offset=0,
+            limit=limit,
+            max_id=0,
+            min_id=0,
+            hash=0
+        ))
+        if not history.messages:
+            break
+        messages = history.messages
+        for message in messages:
+            all_messages.append(message.to_dict())
+        offset_id = messages[len(messages) - 1].id
+        total_messages = len(all_messages)
+        if total_count_limit != 0 and total_messages >= total_count_limit:
+            break
+
+    with open('channel_messages.json', 'w') as outfile:
+        json.dump(all_messages, outfile, cls=DateTimeEncoder)
+
+
+with client:
+    client.loop.run_until_complete(main(phone))
+
+# Scrap Chat Messages
+# chats = ['icospeaks']
+
+# client = TelegramClient('None', api_id, api_hash)
+
+# df = pd.DataFrame()
+
+# for chat in chats:
+#     with TelegramClient('test', api_id, api_hash) as client:
+#         for message in client.iter_messages(chat, offset_date=datetime.date.today() , reverse=True):
+#             print(message)
+#             data = { "group" : chat, "sender" : message.sender_id, "text" : message.text, "date" : message.date}
+
+#             temp_df = pd.DataFrame(data, index=[1])
+#             df = df.append(temp_df)
+
+# df['date'] = df['date'].dt.tz_localize(None)
+# df.to_csv("data_{}.csv".format(datetime.date.today()), index=False)
