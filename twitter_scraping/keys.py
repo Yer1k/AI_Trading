2396dad0fc4b823b5026dbc54d53bdcc2284b741
Scott Lai <43226003+ScottLL@users.noreply.github.com>
2022-11-03 11:58:16 -0400

    Update keys.py

--- twitter_scraping/keys.py
+++ twitter_scraping/keys.py
@@ -1,6 +1,13 @@
-# @MEXC_bot
-consumer_key = 'soBLJ1xLRa7ImUbqf8YyNjMJg'
-consumer_secret = 'LBnJZY6JLkkz5xco2k7oX3R8pl4NVREPKPuP0o3NCKWuCUYUmj'
-Bearer_Token = 'AAAAAAAAAAAAAAAAAAAAALOAfQEAAAAA7VTahTRjc%2B3tm2uN62FvdaxV8X0%3DBXGEd59gOBWIv71mhkqTNHrhD2soByHN07nRZJRT9Y2Pdb7dTm'
-Token = '1551719164372520960-YsN9MtkE6vaa3vQ8HLnSZLeGqC8Yko'
-Token_Secret = 'RBATmiZ7P2dtbUPnNcrSzMk6GEW5h0BsRMhFQBv5VAWgT'
+# Twitter_api_keys
+consumer_key = 'XddwIw3IS0rIBYsWplunbyDrb'
+consumer_secret = '1J2hIm2eBqnOA01ODxYvrtyg0RP6swjkrTf9zFdnc0NiqJQ2Hn'
+Bearer_Token = 'AAAAAAAAAAAAAAAAAAAAAIoKfQEAAAAAd%2BeD%2FOOAirf4LpOxvu3105MHzsA%3DXv2bHs2gd33t7DF0dCyAw01jI93anUfnHEH1Os6tRXIyPIsziH'
+Token = '1551971614765506560-6YYTbNkLa7AcHUgUwp98n6HztNu4Me'
+Token_Secret = 'K6vdkn9O5vw5MowRX0hf2yW6aijDveMI05h4ejp0hcnAm'
+
+# AWS S3 bucket keys
+OPENAI_API_KEY = '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'
+access_key = 'AKIAV6UJAU3DSAWHXZ75'
+secret_access_key = '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'
+Credentials_Access_Key_ID = "AKIAV6UJAU3DSOTQ6HUT"
+Credentials_Secret_Access_Key = "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"
