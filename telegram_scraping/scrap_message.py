ec4ba63a661f1ce9e6a4539b63326566bffb6934
Dingkun Yang <81750079+Yer1k@users.noreply.github.com>
2022-12-14 05:53:31 +0000

    added tel scrape data

--- telegram_scraping/scrap_message.py
+++ telegram_scraping/scrap_message.py
@@ -83,26 +83,11 @@ async def main(phone):
 
     with open('channel_messages.json', 'w') as outfile:
         json.dump(all_messages, outfile, cls=DateTimeEncoder)
+    
+    df = pd.DataFrame(all_messages)
+    df.to_csv('../data_outputs/channel_messages.csv', index=False)
 
 
 with client:
     client.loop.run_until_complete(main(phone))
 
-# Scrap Chat Messages
-# chats = ['icospeaks']
-
-# client = TelegramClient('None', api_id, api_hash)
-
-# df = pd.DataFrame()
-
-# for chat in chats:
-#     with TelegramClient('test', api_id, api_hash) as client:
-#         for message in client.iter_messages(chat, offset_date=datetime.date.today() , reverse=True):
-#             print(message)
-#             data = { "group" : chat, "sender" : message.sender_id, "text" : message.text, "date" : message.date}
-
-#             temp_df = pd.DataFrame(data, index=[1])
-#             df = df.append(temp_df)
-
-# df['date'] = df['date'].dt.tz_localize(None)
-# df.to_csv("data_{}.csv".format(datetime.date.today()), index=False)
